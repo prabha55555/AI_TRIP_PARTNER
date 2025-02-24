d23c3a9920935e538197d5db884bb6f834703afb
Prabhakaran K <125977684+prabha55555@users.noreply.github.com>
2025-02-25 00:57:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
-import Header from './components/custom/header'; // Import the Header component
+import Header from './components/custom/Header'; 
 import Create from './create/index.jsx';
 import './index.css';
 import MyTrips from './my-trips/index.jsx';
